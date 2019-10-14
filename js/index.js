ea1b119c1c910f01dcb0454386abc2af7d443215
Spicy <nicholaspgagnon@gmail.com>
2019-10-14 13:51:25 -0700

    finally

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-ScrollReveal({reset:true, duration: 800}).reveal('.contact');
+ScrollReveal({reset:true, duration: 300}).reveal('.contact');
 ScrollReveal().reveal('.choosr', {delay: 100});
 ScrollReveal().reveal('.nonProf', {delay: 100});
